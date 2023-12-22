84853391ac17d44ea6eff8b75e88b4db3ede19d3
Matiboux <matiboux@gmail.com>
2023-12-22 01:10:05 +0100

    Update i18n function

--- app/app/src/i18n/index.ts
+++ app/app/src/i18n/index.ts
@@ -3,15 +3,18 @@ import fr from './fr'
 
 const locales = { en, fr } as const
 
-function i18n(locale: keyof typeof locales, key: string, ...args: any[]): string
+function i18n(
+	locale: keyof typeof locales,
+	key: keyof typeof locales[typeof locale],
+	...args: string[]
+)
 {
-  	return locales[locale]?.[key]
-		?.replace(/{(\d+)}/g, (match, number) =>
-			typeof args[number] != 'undefined'
-				? args[number]
-				: match
-		)
-		?? key
+	const value = locales[locale]?.[key] ?? key
+	return value.replace(/{(\d+)}/g, (match, number) =>
+		typeof args[number] != 'undefined'
+			? args[number]
+			: match
+	)
 }
 
 export default i18n
