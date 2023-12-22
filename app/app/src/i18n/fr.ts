b76240093a83dfe1d5cb8c49b9d713a5df4cbd0c
Matiboux <matiboux@gmail.com>
2023-12-22 01:28:58 +0100

    Add fr translations

--- app/app/src/i18n/fr.ts
+++ app/app/src/i18n/fr.ts
@@ -3,6 +3,8 @@ import I18n from './type'
 const locale =
 {
 	'What is {0}?': 'Qu\'est-ce que {0} ?',
+	'Visit our website': 'Visite notre site web',
+	'Join our Discord': 'Rejoins notre Discord',
 } as const
 
 // Static type check
