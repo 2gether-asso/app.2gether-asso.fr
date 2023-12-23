dbcabf4c4414471d6ac1b4f9951dc2e7c4f6aba3
Matiboux <matiboux@gmail.com>
2023-12-23 20:30:12 +0100

    Use i18n strings for description

--- app/app/src/i18n/locales/en.ts
+++ app/app/src/i18n/locales/en.ts
@@ -3,6 +3,20 @@ const defaultLocale =
 	'What\'s up on 2GETHER in 2024?',
 	'Visit our website',
 	'Join our Discord',
+	'Over the next few years, 2GETHER is evolving!',
+	'On the agenda:',
+	'The 2GETHER bot always online!',
+	'Recurring giveaways coming back!',
+	'Special private or semi-private channels, called Bubbles!',
+	'The possibility for members to create activities!',
+	'Meet Up 2024',
+	'Events agenda redesign, with a monthly schedule to better organize ourselves!',
+	'Discord server overhaul to include mascots and improve ergonomics',
+	'New event types managed by members or consultants, such as quiz nights',
+	'Step-by-step launch of announced projects during 2MORROW',
+	'An activity time calculation system, transforming your time on 2GETHER into 2GETHER money!',
+	'2COINS will allow you to create activities, rent bubbles, participate in giveaways, place pixels on the place canvas, or other interactions on 2GETHER applications!',
+	'In short, a lot of things are coming in the next few years!',
 ] as const
 
 type Keys = typeof defaultLocale[number]
