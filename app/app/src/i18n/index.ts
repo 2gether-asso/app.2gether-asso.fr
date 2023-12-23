8195db754c0229490430cff30d32a7414077527e
Matiboux <matiboux@gmail.com>
2023-12-23 16:52:56 +0100

    Add custom getLocaleByPath implementation

--- app/app/src/i18n/index.ts
+++ app/app/src/i18n/index.ts
@@ -1,3 +1,5 @@
+import i18nConfig from '/config/i18n'
+
 import en from './locales/en'
 import fr from './locales/fr'
 import I18n, { defaultLocaleKey } from './type'
@@ -31,9 +33,31 @@ function i18nFactory(locale: Parameters<typeof i18n>[0])
 	return (...args: Tail<Parameters<typeof i18n>>) => i18n(locale, ...args)
 }
 
+function getLocaleByPath(path: string): string | undefined
+{
+	for (const locale of i18nConfig.locales)
+	{
+		if (typeof locale !== 'string')
+		{
+			if (locale.path === path)
+			{
+				const code = locale.codes.at(0)
+				return code
+			}
+		}
+		else if (locale === path)
+		{
+			return locale
+		}
+	}
+
+	return undefined
+}
+
 export default i18n
 
 export {
 	defaultLocaleKey,
 	i18nFactory,
+	getLocaleByPath,
 }
