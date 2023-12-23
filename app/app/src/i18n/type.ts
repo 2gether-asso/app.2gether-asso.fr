fe8f40c448398c564d835cfe686ed78aefd87d38
Matiboux <matiboux@gmail.com>
2023-12-23 19:28:27 +0100

    Refactor i18n src files

--- app/app/src/i18n/type.ts
+++ app/app/src/i18n/type.ts
@@ -1,9 +1,16 @@
-import defaultLocale from './locales/en'
+import i18nConfig from '/config/i18n'
+
+import defaultLocaleData from './locales/en'
+
+const defaultLocale = i18nConfig.defaultLocale
 
 const defaultLocaleKey = 'en' as const
 
-type I18n = Readonly<Record<keyof typeof defaultLocale, string>>
+type I18n = Readonly<Record<keyof typeof defaultLocaleData, string>>
 
 export default I18n
 
-export { defaultLocaleKey }
+export {
+	defaultLocale,
+	defaultLocaleKey,
+}
